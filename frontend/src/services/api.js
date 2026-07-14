70e113b04ce260081b89842b3e20ba184a1b7164
jrmugweru <krissfordmwangi@gmail.com>
2026-07-14 14:31:46 +0300

    just heavy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,3 +3,17 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://kisauni-education-1.onrender.com/api",
 });
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default api;
